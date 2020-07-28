ba4fb8123ae2331466c2ac5a8c0501a736627db7
StaticESC <slushiboy21@gmail.com>
2020-07-28 14:46:19 +0530

    Added pistol Animation
    > Added an animator and made a pistol animation.
    > Made an "emtpy animation" called "empty state". (just using hacks atm cuz i dont really know how to use the animator controller).
    > I don't really know about the relation between "Has exit time" and triggers yet. I just play around with params and make it work.

---
+++ Assets/Pistol.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+    Animator myAnimator;
+
+    private void Start()
+    {
+        myAnimator = gameObject.transform.parent.parent.gameObject.GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        PistolShoot();
+    }
+
+    private void PistolShoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && WeaponSetter.GetActiveWeapon() == "Pistol")
+        {
+            myAnimator.SetTrigger("PistolShoot");
+        }
+    }
+}
--- Assets/Scripts/WeaponSetter.cs
+++ Assets/Scripts/WeaponSetter.cs
@@ -6,18 +6,14 @@ public class WeaponSetter : MonoBehaviour
 {
     //cache
     List<GameObject> weapons = new List<GameObject>();
-
-
-    //to be set by a setter method
-
+    static string activeWeapon;
 
     void Start()
     {
         BuildWeaponsList();
         WeaponDeactivator();
+        WeaponSetterMethod(1); //equip barehand
     }
-
-    
 
     void Update()
     {
@@ -32,11 +28,34 @@ public class WeaponSetter : MonoBehaviour
         }
     }
 
+    //TODO: Avoid string references!!!! (Work on this)
     private void WeaponSelector()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) { WeaponSetterMethod(1); }
-        else if (Input.GetKeyDown(KeyCode.Alpha2)) { WeaponSetterMethod(2); }
-        else if (Input.GetKeyDown(KeyCode.Alpha3)) { WeaponSetterMethod(3); }
+        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        { 
+            WeaponSetterMethod(1);
+            SetActiveWeapon("Bare Hand");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) 
+        { 
+            WeaponSetterMethod(2);
+            SetActiveWeapon("Pistol");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) 
+        {
+            WeaponSetterMethod(3);
+            SetActiveWeapon("Sword");
+        }
+    }
+
+    private void SetActiveWeapon(string weaponName)
+    {
+        activeWeapon = weaponName;
+    }
+
+    public static string GetActiveWeapon()
+    {
+        return activeWeapon;
     }
 
     private void WeaponDeactivator()
